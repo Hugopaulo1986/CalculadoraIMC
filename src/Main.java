fd782c3f358a8aad01b7818c56e8185f87cc422a
Hugo Paulo <94487927+Hugopaulo1986@users.noreply.github.com>
2024-03-24 22:51:22 +0100

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu peso: ");
+        double peso = scanner.nextDouble();
+
+        System.out.println("Digite sua altura : ");
+        double altura = scanner.nextDouble();
+
+        double imc = calcularIMC(peso, altura);
+
+        System.out.println("Seu IMC é: " + imc);
+
+        scanner.close();
+    }
+
+    public static double calcularIMC(double peso, double altura) {
+        return peso / (altura * altura);
     }
 }
